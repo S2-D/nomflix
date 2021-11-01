7d3ad993251a22f850fb09a63542b1409382ead0
Sodam Shin <damsoshin@gmail.com>
2021-11-01 17:29:32 +0900

    #6.2 TVPresenter and Loader Components

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,9 +1,41 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Section from "../../Components/Section";
+import Loader from "Components/Loader";
+
+const Container = styled.div`
+  padding: 10px;
+`;
 
 const TVPresenter = ({ topRated, popular, airingToday, loading, error }) =>
-  null;
+  loading ? (
+    <Loader />
+  ) : (
+    <Container>
+      {topRated && topRated.length > 0 && (
+        <Section title="Top Rated Show">
+          {topRated.map((show) => (
+            <span key={show.id}>{show.name}</span>
+          ))}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title="Popluar Show">
+          {popular.map((show) => (
+            <span key={show.id}>{show.name}</span>
+          ))}
+        </Section>
+      )}
+      {airingToday && airingToday.length > 0 && (
+        <Section title="airingToday">
+          {airingToday.map((show) => (
+            <span key={show.id}>{show.name}</span>
+          ))}
+        </Section>
+      )}
+    </Container>
+  );
 
 TVPresenter.propTypes = {
   topRated: PropTypes.array,
