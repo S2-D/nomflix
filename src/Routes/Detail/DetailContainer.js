dfba151f5410673ab19dc9910f576d980f49ee06
Sodam Shin <damsoshin@gmail.com>
2021-11-01 15:52:49 +0900

    #5.5 Detail Container part one

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -7,6 +7,21 @@ export default class extends React.Component {
     loading: true,
     error: null,
   };
+
+  async componentDidMount() {
+    const {
+      match: {
+        params: { id },
+      },
+      history: { push },
+    } = this.props;
+    const parseID = parseInt(id);
+    if (isNaN(parseID)) {
+      return push("/");
+    }
+    console.log(this.props);
+  }
+
   render() {
     const { result, loading, error } = this.state;
     return <DetailPresenter result={result} loading={loading} error={error} />;
